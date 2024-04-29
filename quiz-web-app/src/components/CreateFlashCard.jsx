4e5a90eb173c4d82cf2633137d02a3bd48092b01
shrawaniwagh2003 <waghshrawani2003@gmail.com>
2024-04-29 20:51:45 +0530

    modified alignment

--- quiz-web-app/src/components/CreateFlashCard.jsx
+++ quiz-web-app/src/components/CreateFlashCard.jsx
@@ -4,7 +4,7 @@ import createflash from '../assets/createflsh.png'
 
 function CreateFlashCard() {
   return (
-    <div id="header" className="py-4 mt-8">
+    <div id="header" className="py-4 mt-8 lg:ml-16 lg:mr-16">
       <div className="container mx-auto flex items-center justify-between ">
         <Link to="/">
           <img src={logo2} alt="Logo" className="logo lg:w-48 w-28 justify-start ml-2 " />
